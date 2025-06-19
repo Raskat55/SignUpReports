b5066c3e1a7bdc00b5951566c2838006c6b4f24a
Konstantin Myasnikov <grammatika55@gmail.com>
2025-06-19 13:01:06 +0600

    Registration screen

--- app/src/main/java/com/ortin/signupreport/screen/RegistrationRole.kt
+++ app/src/main/java/com/ortin/signupreport/screen/RegistrationRole.kt
@@ -1,8 +1,28 @@
 package com.ortin.signupreport.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun RegistrationRole(){
+    Column {
 
+        Text(text = "Select your role")
+        Text(text = "for registration")
+//        Button(
+//            onClick = TODO(),
+//            enabled = TODO(),
+//            shape = TODO(),
+//            colors = TODO(),
+//            elevation = TODO(),
+//            border = TODO(),
+//            contentPadding = TODO(),
+//            interactionSource = TODO(),
+//            content = TODO()
+//        )
+
+
+    }
 }
