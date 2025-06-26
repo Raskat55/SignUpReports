25ba958cd41d3931b27d8e24d16c215268662999
Konstantin Myasnikov <grammatika55@gmail.com>
2025-06-26 12:10:31 +0600

    Registration screen

--- app/src/main/java/com/ortin/signupreport/screen/registration/RegistrationRole.kt
+++ app/src/main/java/com/ortin/signupreport/screen/registration/RegistrationRole.kt
@@ -34,9 +34,11 @@ import com.ortin.signupreport.ui.theme.BackgroundColor
 // Main table
 @SuppressLint("ResourceAsColor")
 @Composable
-fun RegistrationRole() {
+fun RegistrationRole(
+    modifier: Modifier = Modifier.background(BackgroundColor)
+) {
     Box(
-        modifier = Modifier
+        modifier
             .fillMaxSize()
             .background(BackgroundColor),
         contentAlignment = Alignment.TopCenter
