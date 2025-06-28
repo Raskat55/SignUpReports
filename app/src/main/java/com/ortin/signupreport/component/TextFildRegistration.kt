0b7e5de1fe8b660d634d8f4e416e0ff6ace7561a
Konstantin Myasnikov <grammatika55@gmail.com>
2025-06-28 17:47:13 +0600

    Add empty line

--- app/src/main/java/com/ortin/signupreport/component/TextFildRegistration.kt
+++ app/src/main/java/com/ortin/signupreport/component/TextFildRegistration.kt
@@ -1,35 +1,30 @@
 package com.ortin.signupreport.component
 
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text.input.rememberTextFieldState
+import android.R.attr.label
+import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 
 
 @Composable
-fun TextFildRegistration(modifier: Modifier = Modifier){
+fun TextFildRegistration() {
 
-   var text by remember { mutableStateOf(TextFieldValue("")) }
+    val textValue = remember { mutableStateOf(" ") }
 
-   OutlinedTextField(
-        state = rememberTextFieldState(),
-        placeholder = {  },
-        label = {  },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Text)
-        )
+    OutlinedTextField(
+        value = textValue,
+        placeholder = Text(""),
+        label = Text("Phone")
+    ){
+
+    }
 
 }
-
 
 
 @Preview
