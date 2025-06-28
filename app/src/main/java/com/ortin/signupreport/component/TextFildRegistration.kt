71ad1ef9a3ed50464c52d653672b45e882d999ed
Konstantin Myasnikov <grammatika55@gmail.com>
2025-06-28 20:12:56 +0600

    Add empty line

--- app/src/main/java/com/ortin/signupreport/component/TextFildRegistration.kt
+++ app/src/main/java/com/ortin/signupreport/component/TextFildRegistration.kt
@@ -1,7 +1,6 @@
 package com.ortin.signupreport.component
 
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -19,9 +18,7 @@ fun TextFildRegistration() {
 
     OutlinedTextField(
         value = textValue.value,
-        onValueChange = onTextChange,
-        placeholder = Text(""),
-        label = Text("Phone")
+        onValueChange = { onTextChange },
     )
 }
 
