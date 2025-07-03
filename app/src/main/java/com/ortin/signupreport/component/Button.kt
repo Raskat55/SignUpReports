c44a1e9048e3a6612ce87a1efb68238ef16d9eda
Konstantin Myasnikov <grammatika55@gmail.com>
2025-07-04 01:37:00 +0600

    Add empty line

--- app/src/main/java/com/ortin/signupreport/component/Button.kt
+++ app/src/main/java/com/ortin/signupreport/component/Button.kt
@@ -1,10 +1,20 @@
 package com.ortin.signupreport.component
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
+
+//@Composable
+//fun advice(modifier: Modifier = Modifier) {
+//    Row {
+//        Box()
+//    }
+//}
 
 //@Preview
 //@Composable
