a45653d7952a567efaabb2ba76ea9456f79e49f5
Konstantin Myasnikov <grammatika55@gmail.com>
2025-07-14 14:01:00 +0600

    Add empty line

--- app/src/main/java/com/ortin/signupreport/screen/registration/CheckMailRegistratoinScreen.kt
+++ app/src/main/java/com/ortin/signupreport/screen/registration/CheckMailRegistratoinScreen.kt
@@ -1,14 +1,11 @@
 package com.ortin.signupreport.screen.registration
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,20 +17,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.LinkAnnotation
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextLinkStyles
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withLink
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ortin.signupreport.component.CheckMailLinkedText
 import com.ortin.signupreport.presentation.component.ConfirmButton
 import com.ortin.signupreport.presentation.component.LogoWithBackground
-import com.ortin.signupreport.presentation.component.MailNumberField
 import com.ortin.signupreport.ui.theme.BackgroundColor
 import com.ortin.signupreport.ui.theme.ButtonColor
 
@@ -68,68 +59,33 @@ fun CheckMailRegistrationScreen(modifier: Modifier = Modifier) {
                         .padding(top = 10.dp, end = 15.dp),
                     fontSize = 14.sp,
                     textAlign = TextAlign.Right,
-                    fontWeight = FontWeight.Bold,
                     color = ButtonColor
                 )
                 Text(
                     "Сheck your e-mail",
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(15.dp),
-                    fontSize = 30.sp,
+                        .padding(10.dp),
+                    fontSize = 25.sp,
                     fontWeight = FontWeight.Bold,
                     textAlign = TextAlign.Center,
                     color = Color.Black
                 )
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
+                Row(modifier = Modifier) {
                     //поля для ввода цифр
-                    MailNumberField()
-                    MailNumberField()
-                    MailNumberField()
-                    MailNumberField()
+
                 }
-                Spacer(
-                    modifier = Modifier
-                        .height(15.dp)
-                )
                 CheckMailLinkedText()
-                Spacer(
-                    modifier = Modifier
-                        .height(310.dp)
-                )
                 ConfirmButton(
                     buttonText = "Confirm",
                     buttonWidth = 320.dp,
-                    buttonHeight = 60.dp,
+                    buttonHeight = 56.dp,
                     modifier = Modifier.fillMaxWidth()
                 )
             }
 
         }
         LogoWithBackground(modifier = Modifier.padding(top = 70.dp))
-        Text(
-            modifier = modifier.padding(
-                bottom = 10.dp,
-                top = 890.dp
-            ),
-            fontSize = 14.sp,
-            fontWeight = FontWeight.Normal,
-            textAlign = TextAlign.Center,
-            text = buildAnnotatedString {
-                withLink(
-                    LinkAnnotation.Url(
-                        url = "https://cloud.avbinvest.com/",
-                        TextLinkStyles(style = SpanStyle(color = ButtonColor))
-                    )
-                ) {
-                    append("Send the letter again")
-                }
-            }
-        )
     }
 }
 
